e783e9efbe9dc7338955b3c39276babc2c374f96
Antonio Caggiano <antonio.caggiano@collabora.com>
2021-07-06 18:09:09 +0200

    Transition hotfix
    Transition the sampled image through PRESENT_SRC to avoid weird
    artifacts possibly related to its compression.

--- ui/console-vk.c
+++ ui/console-vk.c
@@ -43,8 +43,8 @@ bool console_vk_check_format(pixman_format_code_t format)
     }
 }
 
-
-static void vk_command_buffer_submit(VkCommandBuffer* command_buffer, VkQueue queue) {
+static void vk_command_buffer_submit(VkCommandBuffer *command_buffer, VkQueue queue)
+{
     vkEndCommandBuffer(*command_buffer);
 
     VkSubmitInfo submit_info = {
@@ -67,16 +67,14 @@ static void vk_image_transition(VkCommandBuffer cmd_buf, VkImage image, VkImageL
     VkAccessFlags src_access_mask;
     VkAccessFlags dst_access_mask;
 
-    if (old_layout == VK_IMAGE_LAYOUT_UNDEFINED
-        && new_layout == VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL) {
-            // Do not wait for anything before starting the transition
-            src_stage = VK_PIPELINE_STAGE_TOP_OF_PIPE_BIT;
-            src_access_mask = 0;
-            // Complete the transition before transfer stage
-            dst_stage = VK_PIPELINE_STAGE_TRANSFER_BIT;
-            dst_access_mask = VK_ACCESS_TRANSFER_WRITE_BIT;
-    } else if (old_layout == VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL
-               && new_layout == VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL) {
+    if (old_layout == VK_IMAGE_LAYOUT_UNDEFINED && new_layout == VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL) {
+        // Do not wait for anything before starting the transition
+        src_stage = VK_PIPELINE_STAGE_TOP_OF_PIPE_BIT;
+        src_access_mask = 0;
+        // Complete the transition before transfer stage
+        dst_stage = VK_PIPELINE_STAGE_TRANSFER_BIT;
+        dst_access_mask = VK_ACCESS_TRANSFER_WRITE_BIT;
+    } else if (old_layout == VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL && new_layout == VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL) {
         // Wait transfer has finished
         src_stage = VK_PIPELINE_STAGE_TRANSFER_BIT;
         src_access_mask = VK_ACCESS_TRANSFER_WRITE_BIT;
@@ -85,9 +83,14 @@ static void vk_image_transition(VkCommandBuffer cmd_buf, VkImage image, VkImageL
         dst_stage = VK_PIPELINE_STAGE_FRAGMENT_SHADER_BIT;
         dst_access_mask = VK_ACCESS_SHADER_READ_BIT;
     } else {
-        g_assert(false); // TODO FIXME transition not supported for the moment
-    }
+        // Wait transfer has finished
+        src_stage = VK_PIPELINE_STAGE_TRANSFER_BIT;
+        src_access_mask = VK_ACCESS_TRANSFER_WRITE_BIT;
 
+        // Complete layout transition before shader read stage
+        dst_stage = VK_PIPELINE_STAGE_FRAGMENT_SHADER_BIT;
+        dst_access_mask = VK_ACCESS_SHADER_READ_BIT;
+    }
 
     VkImageMemoryBarrier image_memory_barrier = {
         .sType = VK_STRUCTURE_TYPE_IMAGE_MEMORY_BARRIER,
@@ -109,7 +112,6 @@ static void vk_image_transition(VkCommandBuffer cmd_buf, VkImage image, VkImageL
 
     vkCmdPipelineBarrier(cmd_buf, src_stage, dst_stage, 0, 0, NULL, 0, NULL, 1, &image_memory_barrier);
 }
-
 
 static void vk_copy_buffer_to_image(VkCommandBuffer cmd_buf, VkBuffer buffer, uint32_t buffer_row_length, VkImage image, uint32_t x, uint32_t y, uint32_t width, uint32_t height)
 {
@@ -138,6 +140,25 @@ static void vk_copy_buffer_to_image(VkCommandBuffer cmd_buf, VkBuffer buffer, ui
     vkCmdCopyBufferToImage(cmd_buf, buffer, image, VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL, 1, &buffer_image_copy);
 }
 
+static VkImageTiling get_tiling_for_format(VkPhysicalDevice physical_device, VkFormat format)
+{
+    VkFormatProperties format_properties;
+    vkGetPhysicalDeviceFormatProperties(physical_device, format, &format_properties);
+
+    VkFormatFeatureFlags format_features = VK_FORMAT_FEATURE_SAMPLED_IMAGE_BIT;
+
+    if (format_properties.optimalTilingFeatures & format_features) {
+        return VK_IMAGE_TILING_OPTIMAL;
+    }
+
+    if (format_properties.linearTilingFeatures & format_features) {
+        return VK_IMAGE_TILING_LINEAR;
+    }
+
+    g_assert(false);
+    return 0;
+}
+
 // TODO: Is this the swapchain image?
 /// I believe this is a texture created to upload data from the display surface
 /// This texture will probably be rendered later with the blit pipelines.
@@ -161,20 +182,26 @@ void surface_vk_create_texture(QEMUVkDevice device,
         g_assert_not_reached();
     }
 
+    uint32_t width = surface_width(surface);
+    uint32_t height = surface_height(surface);
+
+    VkImageUsageFlags usage = VK_IMAGE_USAGE_TRANSFER_DST_BIT | VK_IMAGE_USAGE_SAMPLED_BIT;
+    VkImageTiling tiling = get_tiling_for_format(device.physical_device.handle, surface->vk_format);
+
     VkImageCreateInfo image_create_info = {
         .sType = VK_STRUCTURE_TYPE_IMAGE_CREATE_INFO,
         .imageType = VK_IMAGE_TYPE_2D,
         .format = surface->vk_format,
         .extent = {
-            .width = surface_width(surface),
-            .height = surface_height(surface),
+            .width = width,
+            .height = height,
             .depth = 1,
         },
         .mipLevels = 1,
         .arrayLayers = 1,
         .samples = VK_SAMPLE_COUNT_1_BIT,
-        .tiling = VK_IMAGE_TILING_OPTIMAL,
-        .usage = VK_IMAGE_USAGE_TRANSFER_DST_BIT | VK_IMAGE_USAGE_SAMPLED_BIT, // TODO: figure out usage
+        .tiling = tiling,
+        .usage = usage,
         .sharingMode = VK_SHARING_MODE_EXCLUSIVE,
         .initialLayout = VK_IMAGE_LAYOUT_UNDEFINED,
     };
@@ -218,8 +245,6 @@ void surface_vk_create_texture(QEMUVkDevice device,
 
     VK_CHECK(vkCreateSampler(device.handle, &sampler_create_info, NULL, &surface->vk_sampler));
 
-    uint32_t width = surface_width(surface);
-    uint32_t height = surface_height(surface);
     surface_vk_update_texture(device, surface, 0, 0, width, height);
 }
 
@@ -227,9 +252,7 @@ void surface_vk_update_texture(QEMUVkDevice device,
                                DisplaySurface *surface,
                                int x, int y, int w, int h)
 {
-    uint8_t *pixel_data = (void *)surface_data(surface)
-        + surface_stride(surface) * y
-        + surface_bytes_per_pixel(surface) * x;
+    uint8_t *pixel_data = (void *)surface_data(surface) + surface_stride(surface) * y + surface_bytes_per_pixel(surface) * x;
 
     uint32_t width = w;
     uint32_t height = h;
@@ -237,7 +260,7 @@ void surface_vk_update_texture(QEMUVkDevice device,
     QEMUVkBuffer staging_buffer = vk_create_buffer(device, surface_size, VK_BUFFER_USAGE_TRANSFER_SRC_BIT,
                                                    VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT);
 
-    void* data;
+    void *data;
     VK_CHECK(vkMapMemory(device.handle, staging_buffer.memory, 0, surface_size, 0, &data));
     memcpy(data, pixel_data, surface_size);
     vkUnmapMemory(device.handle, staging_buffer.memory);
@@ -249,30 +272,26 @@ void surface_vk_update_texture(QEMUVkDevice device,
     // in texels
     uint32_t buffer_row_length = surface_stride(surface) / surface_bytes_per_pixel(surface);
     vk_copy_buffer_to_image(device.general_command_buffer, staging_buffer.handle, buffer_row_length, surface->vk_image, x, y, width, height);
-    vk_image_transition(device.general_command_buffer, surface->vk_image, VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL, VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL);
+
+    // This double transition is a workaround for intel framebuffer compression
+    // Going directly from TRANSFER_DST_OPTIMAL to SHADER_READ_ONLY_OPTIMAL would result in reading weird artifacts
+    vk_image_transition(device.general_command_buffer, surface->vk_image, VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL, VK_IMAGE_LAYOUT_PRESENT_SRC_KHR);
+    vk_image_transition(device.general_command_buffer, surface->vk_image, VK_IMAGE_LAYOUT_PRESENT_SRC_KHR, VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL);
 
     vk_command_buffer_submit(&device.general_command_buffer, device.graphics_queue);
 
     vk_destroy_buffer(device.handle, staging_buffer);
 }
 
-static VkSemaphore vk_create_semaphore(VkDevice device) {
-    VkSemaphoreCreateInfo semaphore_create_info = {
-        .sType = VK_STRUCTURE_TYPE_SEMAPHORE_CREATE_INFO
-    };
-    VkSemaphore semaphore;
-    VK_CHECK(vkCreateSemaphore(device, &semaphore_create_info, NULL, &semaphore));
-    return semaphore;
-}
 
 void surface_vk_render_texture(QEMUVkDevice device,
-                               QEMUVkSwapchain* swapchain,
+                               QEMUVkSwapchain *swapchain,
                                QEMUVkFrames *frames,
                                QEMUVulkanShader *vks,
                                DisplaySurface *surface)
 {
     assert(vks);
-    
+
     VkDescriptorImageInfo desc_image_info = {
         .imageView = surface->vk_view,
         .imageLayout = VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL,
@@ -293,28 +312,24 @@ void surface_vk_render_texture(QEMUVkDevice device,
 
     vkUpdateDescriptorSets(device.handle, 1, &write_desc_set, 0, NULL);
 
-    VkClearValue clear = {
+    const VkClearValue clear = {
         .color = {
-            .float32 = {0.1f, 0.1f, 0.1f, 1.0f}
+            .float32 = { 0.1f, 0.1f, 0.1f, 0.0f }
         },
     };
 
-    VkRenderPassBeginInfo render_begin_info = {
+    const VkRenderPassBeginInfo render_begin_info = {
         .sType = VK_STRUCTURE_TYPE_RENDER_PASS_BEGIN_INFO,
         .renderPass = vks->texture_blit_render_pass,
         .framebuffer = frames->framebuffers[frames->frame_index],
         .renderArea = {
-            .extent = {
-                .width = swapchain->extent.width,
-                .height = swapchain->extent.height,
-            }
+            .extent = swapchain->extent
         },
         .clearValueCount = 1,
         .pClearValues = &clear,
     };
 
     VkCommandBuffer cmd_buf = frames->cmd_bufs[frames->frame_index];
-    
     vkCmdBeginRenderPass(cmd_buf, &render_begin_info, VK_SUBPASS_CONTENTS_INLINE);
 
     vkCmdBindDescriptorSets(cmd_buf, VK_PIPELINE_BIND_POINT_GRAPHICS, vks->texture_blit_pipeline_layout, 0, 1, &desc_set, 0, NULL);
@@ -325,80 +340,7 @@ void surface_vk_render_texture(QEMUVkDevice device,
 
     VK_CHECK(vkEndCommandBuffer(cmd_buf));
 
-    VkSemaphore image_available_semaphore = vk_create_semaphore(device.handle);
-    VkSemaphore render_finished_semaphore = vk_create_semaphore(device.handle);
 
-    uint32_t image_index;
-    // Image available sempahore will be signaled once the image is correctly acquired
-    VkResult acquire_res = vkAcquireNextImageKHR(device.handle, swapchain->handle, UINT64_MAX, image_available_semaphore, VK_NULL_HANDLE, &image_index);
-
-    if (acquire_res == VK_ERROR_OUT_OF_DATE_KHR) {
-        // TODO extract to function
-        vk_swapchain_recreate(device, swapchain, 0, 0);
-
-        for (uint32_t i = 0; i < swapchain->image_count; ++i) {
-            vkDestroyFramebuffer(device.handle, frames->framebuffers[i], NULL);
-            frames->framebuffers[i] = vk_create_framebuffer(device.handle, vks->texture_blit_render_pass, swapchain->views[i], swapchain->extent.width, swapchain->extent.height);
-        }
-
-        frames->frame_index = 0;
-
-        // TODO Skip this frame? Try acquiring the swapchain again?
-        return;
-    }
-
-    // Wait the swapchain image has been acquired before writing color onto it
-    VkSemaphore wait_semaphores[] = { image_available_semaphore };
-    VkPipelineStageFlags wait_stages[] = { VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT };
-
-    // Signal this semaphore once the commands have finished execution
-    VkSemaphore signal_semaphores[] = { render_finished_semaphore };
-
-    VkSubmitInfo submit_info = {
-        .sType = VK_STRUCTURE_TYPE_SUBMIT_INFO,
-        .waitSemaphoreCount = 1,
-        .pWaitSemaphores = wait_semaphores,
-        .pWaitDstStageMask = wait_stages,
-        .commandBufferCount = 1,
-        .pCommandBuffers = &cmd_buf,
-        .signalSemaphoreCount = 1,
-        .pSignalSemaphores = signal_semaphores,
-    };
-
-    VK_CHECK(vkQueueSubmit(device.graphics_queue, 1, &submit_info, VK_NULL_HANDLE));
-    
-    VK_CHECK(vkQueueWaitIdle(device.graphics_queue));
-
-    VkPresentInfoKHR present_info = {
-        .sType = VK_STRUCTURE_TYPE_PRESENT_INFO_KHR,
-        .waitSemaphoreCount = 1,
-        // Wait for commands to finish before presenting the result
-        .pWaitSemaphores = signal_semaphores,
-        .swapchainCount = 1,
-        .pSwapchains = &swapchain->handle,
-        .pImageIndices = &image_index,
-    };
-
-    VkResult present_res = vkQueuePresentKHR(device.present_queue, &present_info);
-    if (present_res == VK_ERROR_OUT_OF_DATE_KHR) {
-        vk_swapchain_recreate(device, swapchain, 0, 0);
-
-        for (uint32_t i = 0; i < swapchain->image_count; ++i) {
-            vkDestroyFramebuffer(device.handle, frames->framebuffers[i], NULL);
-            frames->framebuffers[i] = vk_create_framebuffer(device.handle, vks->texture_blit_render_pass, swapchain->views[i], swapchain->extent.width, swapchain->extent.height);
-        }
-
-        frames->frame_index = 0;
-        return;
-    }
-
-    VK_CHECK(vkQueueWaitIdle(device.present_queue));
-
-    vkDestroySemaphore(device.handle, image_available_semaphore, NULL);
-    vkDestroySemaphore(device.handle, render_finished_semaphore, NULL);
-
-    // Advance to the next frame
-    frames->frame_index = (frames->frame_index + 1) % swapchain->image_count;
 }
 
 void surface_vk_destroy_texture(VkDevice device,
@@ -407,7 +349,7 @@ void surface_vk_destroy_texture(VkDevice device,
     if (!surface || surface->vk_image == VK_NULL_HANDLE) {
         return;
     }
-    
+
     vkDestroySampler(device, surface->vk_sampler, NULL);
     surface->vk_sampler = VK_NULL_HANDLE;
     vkDestroyImageView(device, surface->vk_view, NULL);
@@ -420,6 +362,7 @@ void surface_vk_setup_viewport(VkCommandBuffer cmdbuf,
                                DisplaySurface *surface,
                                int ww, int wh)
 {
+    // TODO remove this multiplication as it's just a workaround for my desktop
     ww *= 3;
     wh *= 3;
 
